382828ce88691d02d48477cef162d385c401be35
Adrian Brown <153460878+adrianbrown732@users.noreply.github.com>
2024-05-09 21:03:46 -0400

    Create setSquares()

--- script.js
+++ script.js
@@ -68,7 +68,9 @@ function setX() {
   icon.appendChild(rect1);
   icon.appendChild(rect2);
 
-  this.appendChild(icon);
+  switchPlayer();
+
+  return icon;
 }
 
 function setO() {
@@ -89,15 +91,35 @@ function setO() {
 
   icon.appendChild(ellipse);
 
-  this.appendChild(icon);
+  switchPlayer();
+
+  return icon;
+}
+const players = ["player1", "player2"];
+
+let activePlayer = players[0];
+
+const squares = document.querySelectorAll("[data-square]");
+squares.forEach((square) => {
+  square.addEventListener("click", () => {
+    if (square.getAttribute("data-square", "true")) return;
+    activePlayer === "player1"
+      ? square.appendChild(setX())
+      : square.appendChild(setO());
+    square.setAttribute("data-square", "true");
+  });
+});
+
+function setSquares(square) {
+  activePlayer === "player1"
+    ? square.appendChild(setX())
+    : square.appendChild(setO());
 }
 
-const testButton = document.querySelector(".testPush");
-testButton.addEventListener("click", setO);
-
 function switchPlayer() {
-  const activePlayer = document.querySelector(".active-player");
-  activePlayer.classList.toggle("switch-player");
+  const playerIcon = document.querySelector(".active-player");
+  playerIcon.classList.toggle("switch-player");
+  activePlayer = activePlayer === players[0] ? players[1] : players[0];
 }
 
 const button = document.querySelector("button");
@@ -111,58 +133,44 @@ function setPlayer2() {
 }
 
 function GameController() {
-  const players = [setPlayer1(), setPlayer2()];
-  let activePlayer = players[0];
-
-  const board = GameBoard();
-
-  const coordinates = ["a1", "a2", "a3", "b1", "b2", "b3", "c1", "c2", "c3"];
-
-  const selection = [];
-
-  selectio, (n["a1"] = board[0][0]);
-  selection["a2"] = board[0][1];
-  selection["a3"] = board[0][2];
-  selection["b1"] = board[1][0];
-  selection["b2"] = board[1][1];
-  selection["b3"] = board[1][2];
-  selection["c1"] = board[2][0];
-  selection["c2"] = board[2][1];
-  selection["c3"] = board[2][2];
-
-  function printBoard() {
-    const currentBoard = board.map((row) => row.map((cell) => cell.getValue()));
-    console.log(currentBoard);
-  }
-
-  function getPlayerInput() {
-    let playerSelection;
-
-    const isUndefined = (currentValue) => currentValue !== playerSelection;
-
-    do {
-      playerSelection = prompt("Enter coordinate: ");
-    } while (coordinates.every(isUndefined));
-
-    return playerSelection;
-  }
-
-  function setPlayerInput() {
-    console.log(`${activePlayer.getName()}'s turn`);
-    setMove(activePlayer, selection[getPlayerInput()]);
-  }
-
-  function switchPlayers() {
-    activePlayer = activePlayer === players[0] ? players[1] : players[0];
-  }
-
-  printBoard();
-
-  setPlayerInput();
-  printBoard();
-  switchPlayers();
-
-  setPlayerInput();
-  printBoard();
-  switchPlayers();
+  //   const players = [setPlayer1(), setPlayer2()];
+  //   let activePlayer = players[0];
+  //   const board = GameBoard();
+  //   const coordinates = ["a1", "a2", "a3", "b1", "b2", "b3", "c1", "c2", "c3"];
+  //   const selection = [];
+  //   selectio, (n["a1"] = board[0][0]);
+  //   selection["a2"] = board[0][1];
+  //   selection["a3"] = board[0][2];
+  //   selection["b1"] = board[1][0];
+  //   selection["b2"] = board[1][1];
+  //   selection["b3"] = board[1][2];
+  //   selection["c1"] = board[2][0];
+  //   selection["c2"] = board[2][1];
+  //   selection["c3"] = board[2][2];
+  //   function printBoard() {
+  //     const currentBoard = board.map((row) => row.map((cell) => cell.getValue()));
+  //     console.log(currentBoard);
+  //   }
+  //   function getPlayerInput() {
+  //     let playerSelection;
+  //     const isUndefined = (currentValue) => currentValue !== playerSelection;
+  //     do {
+  //       playerSelection = prompt("Enter coordinate: ");
+  //     } while (coordinates.every(isUndefined));
+  //     return playerSelection;
+  //   }
+  //   function setPlayerInput() {
+  //     console.log(`${activePlayer.getName()}'s turn`);
+  //     setMove(activePlayer, selection[getPlayerInput()]);
+  //   }
+  //   function switchPlayers() {
+  //     activePlayer = activePlayer === players[0] ? players[1] : players[0];
+  //   }
+  //   printBoard();
+  //   setPlayerInput();
+  //   printBoard();
+  //   switchPlayers();
+  //   setPlayerInput();
+  //   printBoard();
+  //   switchPlayers();
 }
