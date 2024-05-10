97477f835e939e2058207fe31770629a9c9f60bb
Adrian Brown <153460878+adrianbrown732@users.noreply.github.com>
2024-05-10 18:28:21 -0400

    Create checkForWinnerX()

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function playTicTacToe() {
   const players = ["player1", "player2"];
   const squares = document.querySelectorAll("[data-square]");
-  const button = document.querySelector("button");
+  const resetButton = document.querySelector(".reset");
   const activeIconParent = document.querySelector(".active-player");
   const activeIcon = document.querySelector(".active-player > div");
   let activePlayer = players[0];
@@ -27,7 +27,7 @@ function playTicTacToe() {
     activeIcon.style.opacity = "1";
   };
 
-  button.addEventListener("click", resetGame);
+  resetButton.addEventListener("click", resetGame);
 
   squares.forEach((square) => {
     square.addEventListener("click", () => {
@@ -36,13 +36,13 @@ function playTicTacToe() {
       if (activePlayer === "player1") {
         square.appendChild(setX());
         square.setAttribute("data-icon", "1");
-        console.log(square.getAttribute("data-icon"));
       } else {
         square.appendChild(setO());
         square.setAttribute("data-icon", "2");
-        console.log(square.getAttribute("data-icon"));
       }
       square.setAttribute("data-square", "true");
+
+      checkForWinnerX();
     });
   });
 
@@ -107,6 +107,184 @@ function playTicTacToe() {
     return icon;
   }
 
+  function checkForWinnerX() {
+    const row1 = [squares[0], squares[1], squares[2]];
+
+    const winRow1 =
+      row1[0].getAttribute("data-icon") == "1" &&
+      row1[1].getAttribute("data-icon") == "1" &&
+      row1[2].getAttribute("data-icon") === "1"
+        ? true
+        : false;
+
+    if (winRow1) {
+      row1.forEach((cell) => {
+        cell.setAttribute("data-win", "true");
+      });
+
+      const children = document.querySelectorAll("[data-win='true'] > .icon");
+
+      children.forEach((child) => {
+        child.style.fill = "var(--green)";
+      });
+    }
+
+    const row2 = [squares[3], squares[4], squares[5]];
+
+    const winRow2 =
+      row2[0].getAttribute("data-icon") == "1" &&
+      row2[1].getAttribute("data-icon") == "1" &&
+      row2[2].getAttribute("data-icon") === "1"
+        ? true
+        : false;
+
+    if (winRow2) {
+      row2.forEach((cell) => {
+        cell.setAttribute("data-win", "true");
+      });
+
+      const children = document.querySelectorAll("[data-win='true'] > .icon");
+
+      children.forEach((child) => {
+        child.style.fill = "var(--green)";
+      });
+    }
+
+    const row3 = [squares[6], squares[7], squares[8]];
+
+    const winRow3 =
+      row3[0].getAttribute("data-icon") == "1" &&
+      row3[1].getAttribute("data-icon") == "1" &&
+      row3[2].getAttribute("data-icon") === "1"
+        ? true
+        : false;
+
+    if (winRow3) {
+      row3.forEach((cell) => {
+        cell.setAttribute("data-win", "true");
+      });
+
+      const children = document.querySelectorAll("[data-win='true'] > .icon");
+
+      children.forEach((child) => {
+        child.style.fill = "var(--green)";
+      });
+    }
+
+    const col1 = [squares[0], squares[3], squares[6]];
+
+    const winCol1 =
+      col1[0].getAttribute("data-icon") == "1" &&
+      col1[1].getAttribute("data-icon") == "1" &&
+      col1[2].getAttribute("data-icon") === "1"
+        ? true
+        : false;
+
+    if (winCol1) {
+      col1.forEach((cell) => {
+        cell.setAttribute("data-win", "true");
+      });
+
+      const children = document.querySelectorAll("[data-win='true'] > .icon");
+
+      children.forEach((child) => {
+        child.style.fill = "var(--green)";
+      });
+    }
+
+    const col2 = [squares[1], squares[4], squares[7]];
+
+    const winCol2 =
+      col2[0].getAttribute("data-icon") == "1" &&
+      col2[1].getAttribute("data-icon") == "1" &&
+      col2[2].getAttribute("data-icon") === "1"
+        ? true
+        : false;
+
+    if (winCol2) {
+      col2.forEach((cell) => {
+        cell.setAttribute("data-win", "true");
+      });
+
+      const children = document.querySelectorAll("[data-win='true'] > .icon");
+
+      children.forEach((child) => {
+        child.style.fill = "var(--green)";
+      });
+    }
+
+    const col3 = [squares[2], squares[5], squares[8]];
+
+    const winCol3 =
+      col3[0].getAttribute("data-icon") == "1" &&
+      col3[1].getAttribute("data-icon") == "1" &&
+      col3[2].getAttribute("data-icon") === "1"
+        ? true
+        : false;
+
+    if (winCol3) {
+      col3.forEach((cell) => {
+        cell.setAttribute("data-win", "true");
+      });
+
+      const children = document.querySelectorAll("[data-win='true'] > .icon");
+
+      children.forEach((child) => {
+        child.style.fill = "var(--green)";
+      });
+    }
+
+    const diag1 = [squares[0], squares[4], squares[8]];
+
+    const winDiag1 =
+      diag1[0].getAttribute("data-icon") == "1" &&
+      diag1[1].getAttribute("data-icon") == "1" &&
+      diag1[2].getAttribute("data-icon") === "1"
+        ? true
+        : false;
+
+    if (winDiag1) {
+      diag1.forEach((cell) => {
+        cell.setAttribute("data-win", "true");
+      });
+
+      const children = document.querySelectorAll("[data-win='true'] > .icon");
+
+      children.forEach((child) => {
+        child.style.fill = "var(--green)";
+      });
+    }
+
+    const diag2 = [squares[2], squares[4], squares[6]];
+
+    const winDiag2 =
+      diag2[0].getAttribute("data-icon") == "1" &&
+      diag2[1].getAttribute("data-icon") == "1" &&
+      diag2[2].getAttribute("data-icon") === "1"
+        ? true
+        : false;
+
+    if (winDiag2) {
+      diag2.forEach((cell) => {
+        cell.setAttribute("data-win", "true");
+      });
+
+      const children = document.querySelectorAll("[data-win='true'] > .icon");
+
+      children.forEach((child) => {
+        child.style.fill = "var(--green)";
+      });
+    }
+
+    console.log(`winRow1: ${winRow1}`);
+    // console.log(`winRow2: ${winRow2}`);
+    // console.log(`winRow3: ${winRow3}`);
+
+    // console.log(`winCol1: ${winCol1}`);
+    // console.log(`winCol2: ${winCol2}`);
+    // console.log(`winCol3: ${winCol3}`);
+  }
+
   function resetGame() {
     squares.forEach((square) => {
       if (square.getAttribute("data-square", "true")) {
@@ -116,11 +294,18 @@ function playTicTacToe() {
           icon.style.opacity = "0";
         });
 
+        square.removeAttribute("data-icon");
+
         setTimeout(() => {
           const child = document.querySelector("div > .icon");
           square.removeChild(child);
           square.setAttribute("data-square", "");
-        }, 500);
+
+          const winningSquares = document.querySelectorAll("[data-win='true']");
+          winningSquares.forEach((winningSquare) => {
+            winningSquare.removeAttribute("data-win");
+          });
+        }, 200);
 
         if (activePlayer === players[1]) {
           activePlayer = players[0];
